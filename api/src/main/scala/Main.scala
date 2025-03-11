6684cde0f6338859115f85d7b7b87693f924a14f
Dennis Hunziker <dennis.hunziker@agoda.com>
2025-03-11 15:49:15 +0700

    Read PORT from env

--- api/src/main/scala/Main.scala
+++ api/src/main/scala/Main.scala
@@ -8,17 +8,23 @@ import org.http4s.ember.server.*
 import org.typelevel.log4cats.LoggerFactory
 import org.typelevel.log4cats.slf4j.Slf4jFactory
 
+import scala.sys.env
+
 object Main extends IOApp {
 
   implicit val loggerFactory: LoggerFactory[IO] = Slf4jFactory.create[IO]
 
-  def run(args: List[String]): IO[ExitCode] =
+  def run(args: List[String]): IO[ExitCode] = {
+    val port = Option(env("PORT"))
+      .flatMap(Port.fromString)
+      .getOrElse(port"8080")
     EmberServerBuilder
       .default[IO]
       .withHost(ipv4"0.0.0.0")
-      .withPort(port"8080")
+      .withPort(port)
       .withHttpApp(helloWorldRoutes)
       .build
       .use(_ => IO.never)
       .as(ExitCode.Success)
+  }
 }
