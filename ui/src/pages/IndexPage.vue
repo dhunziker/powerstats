8e9f88c68f6762d61d9973a7757dcbbcec6d3ef3
Dennis Hunziker <dennis.hunziker@agoda.com>
2025-04-25 21:13:52 +0700

    Redirect users to settings when logged in

--- ui/src/pages/IndexPage.vue
+++ ui/src/pages/IndexPage.vue
@@ -1,16 +1,24 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <login-component
-      :is-new-user="props.isNewUser"
-      active
-    ></login-component>
+    <login-component :is-new-user="props.isNewUser" />
   </q-page>
 </template>
 
 <script setup lang="ts">
-  import LoginComponent from 'components/LoginComponent.vue';
+import LoginComponent from 'components/LoginComponent.vue';
+import { useUserStore } from 'stores/user';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 
-  const props = defineProps({
-    isNewUser: Boolean
-  })
+const props = defineProps({
+  isNewUser: Boolean,
+});
+const store = useUserStore();
+const router = useRouter();
+
+onMounted(() => {
+  if (store.user) {
+    return router.push('/settings');
+  }
+});
 </script>
