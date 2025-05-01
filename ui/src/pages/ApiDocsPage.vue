6b9cc7b6459ec32bd1c52f41f6841e1d339f1367
Dennis Hunziker <dennis.hunziker@agoda.com>
2025-05-01 22:00:17 +0700

    Fixed docs URL

--- ui/src/pages/ApiDocsPage.vue
+++ ui/src/pages/ApiDocsPage.vue
@@ -1,7 +1,8 @@
 <template>
-  <api-docs-component url="http://localhost:8080/docs/docs.yaml" />
+  <api-docs-component :url=apiDocsUrl />
 </template>
 
 <script setup lang="ts">
 import ApiDocsComponent from 'components/ApiDocsComponent.vue';
+const apiDocsUrl = `${process.env.API_BASE_URL}/docs/docs.yaml`;
 </script>
