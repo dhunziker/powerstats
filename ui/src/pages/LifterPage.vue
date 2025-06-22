174c51d99a351263119fc777b44d8c2c71b4a53c
Dennis Hunziker <dennis.hunziker@agoda.com>
2025-06-22 19:57:21 +0700

    Added loading screen to lifter page

--- ui/src/pages/LifterPage.vue
+++ ui/src/pages/LifterPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <q-page class="flex">
-      <div class="q-pa-md q-gutter-md wide-page-content">
+      <div v-if="!$q.loading.isActive" class="q-pa-md q-gutter-md wide-page-content">
         <div class="row items-center">
           <div class="text-h5">{{ name }} ({{ sex }})</div>
         </div>
@@ -19,7 +19,9 @@ import type { PersonalBest, CompetitionResult } from '../services/lifterService'
 import  { findPersonalBests, findCompetitionResults } from '../services/lifterService';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { useQuasar } from 'quasar';
 
+const $q = useQuasar();
 const route = useRoute();
 const name = ref<string>(route.params.name as string);
 const sex = ref<string>('');
@@ -27,16 +29,14 @@ const personalBests = ref<PersonalBest[]>([]);
 const competitionResults = ref<CompetitionResult[]>([]);
 
 onMounted(async () => {
-  await findPersonalBests(name.value).then((response) => {
-    // const lifter = response.data
-    // chartData.series = [lifter.best3SquatKg || 0, lifter.best3BenchKg || 0, lifter.best3DeadliftKg || 0];
-    personalBests.value = response.data
-    sex.value = personalBests.value[0]?.sex || '';
-  });
-  await findCompetitionResults(name.value).then((response) => {
-    // const lifter = response.data
-    // chartData.series = [lifter.best3SquatKg || 0, lifter.best3BenchKg || 0, lifter.best3DeadliftKg || 0];
-    competitionResults.value = response.data
-  });
+  $q.loading.show();
+  await Promise.all([
+    findPersonalBests(name.value),
+    findCompetitionResults(name.value)
+  ]).then(([personalBestsResponse, competitionResultsResponse]) => {
+    sex.value = personalBestsResponse.data[0]?.sex || '';
+    personalBests.value = personalBestsResponse.data
+    competitionResults.value = competitionResultsResponse.data
+  }).finally(() => $q.loading.hide());
 });
 </script>
